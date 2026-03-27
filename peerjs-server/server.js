99c2e1ba103792829a60001c01b77584d28d3f77
Chiff-Evans <evansonmuchai2016@gmail.com>
2026-03-27 15:34:45 +0300

    changes

--- peerjs-server/server.js
+++ peerjs-server/server.js
@@ -2,14 +2,14 @@ const { PeerServer } = require("peer");
 const express = require("express");
 const app = express();
 
-const server = app.listen(process.env.PORT || 9000);
+const server = app.listen(process.env.PORT || 9001);
 
 const peerServer = PeerServer({
-  port: process.env.PORT || 9000,
+  port: process.env.PORT || 9001,
   path: "/peerjs",
   proxied: true,
 });
 
 app.use("/peerjs", peerServer);
 
-console.log("PeerJS server running on port", process.env.PORT || 9000);
+console.log("PeerJS server running on port", process.env.PORT || 9001);
