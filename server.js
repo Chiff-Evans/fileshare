a2832cd816f3080a574fc567f98465324d8ccdeb
Chiff-Evans <evansonmuchai2016@gmail.com>
2026-03-30 11:29:46 +0300

    server fixes

--- server.js
+++ server.js
@@ -1,25 +1,15 @@
 const { PeerServer } = require("peer");
-const express = require("express");
-const app = express();
+const http = require("http");
 
 const PORT = process.env.PORT || 9999;
-const server = app.listen(PORT);
-
-// Enable CORS
-app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Methods", "GET, POST, OPTIONS");
-  res.header("Access-Control-Allow-Headers", "Content-Type");
-  if (req.method === "OPTIONS") {
-    res.sendStatus(200);
-  } else {
-    next();
-  }
-});
 
 const peerServer = PeerServer({
-  server: server,
+  port: PORT,
   path: "/peerjs",
+  cors: {
+    origin: "*",
+    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+  },
 });
 
 console.log("PeerJS server running on port", PORT);
