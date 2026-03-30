13a5dacc3411e25929a1fe7a4a24e9b5c75d93d6
Chiff-Evans <evansonmuchai2016@gmail.com>
2026-03-30 11:15:23 +0300

    enable cors

--- server.js
+++ server.js
@@ -5,6 +5,18 @@ const app = express();
 const PORT = process.env.PORT || 9999;
 const server = app.listen(PORT);
 
+// Enable CORS
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Methods", "GET, POST, OPTIONS");
+  res.header("Access-Control-Allow-Headers", "Content-Type");
+  if (req.method === "OPTIONS") {
+    res.sendStatus(200);
+  } else {
+    next();
+  }
+});
+
 const peerServer = PeerServer({
   server: server,
   path: "/peerjs",
